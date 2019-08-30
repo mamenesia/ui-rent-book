94a11a1a603cc7d95c2c14aaa7e25d7d396a6846
mamenesia <dedy.prasetyo.h@gmail.com>
2019-08-31 01:29:21 +0700

    back to https

--- src/Public/Actions/books.js
+++ src/Public/Actions/books.js
@@ -14,16 +14,16 @@ export const getBooks = (
   const queryLimit = (limit !== null) ? `&limit=${limit}` : '';
   return {
     type: 'GET_BOOKS',
-    // payload: Axios.get(`http://localhost:8080/books?page=${page}&sort=${sort}&order=${order}${querySearch}${queryGenre}${queryLimit}` || `http://remotemysql.com:3306/books?page=${page}&sort=${sort}&order=${order}${querySearch}${queryGenre}${queryLimit}`)
-    payload: Axios.get(`http://remotemysql.com:3306/books?page=${page}&sort=${sort}&order=${order}${querySearch}${queryGenre}${queryLimit}`)
+    // payload: Axios.get(`http://localhost:8080/books?page=${page}&sort=${sort}&order=${order}${querySearch}${queryGenre}${queryLimit}` || `https://remotemysql.com:3306/books?page=${page}&sort=${sort}&order=${order}${querySearch}${queryGenre}${queryLimit}`)
+    payload: Axios.get(`https://remotemysql.com:3306/books?page=${page}&sort=${sort}&order=${order}${querySearch}${queryGenre}${queryLimit}`)
   };
 };
 
 export const addBook = (title, image, genre, desc, released_at, available) => {
   return {
     type: 'POST_BOOK',
-    // payload: Axios.post(`http://localhost:8080/books` || 'http://remotemysql.com:3306/books', {
-    payload: Axios.post('http://remotemysql.com:3306/books', {
+    // payload: Axios.post(`http://localhost:8080/books` || 'https://remotemysql.com:3306/books', {
+    payload: Axios.post('https://remotemysql.com:3306/books', {
       title,
       image,
       genre,
@@ -37,8 +37,8 @@ export const addBook = (title, image, genre, desc, released_at, available) => {
 export const updateBook = (book_id, title, image, genre, desc) => {
   return {
     type: 'UPDATE_BOOK',
-    // payload: Axios.patch(`http: //localhost:8080/books/${book_id}` || `http://remotemysql.com:3306/books/${book_id}`, {
-    payload: Axios.patch(`http://remotemysql.com:3306/books/${book_id}`, {
+    // payload: Axios.patch(`http: //localhost:8080/books/${book_id}` || `https://remotemysql.com:3306/books/${book_id}`, {
+    payload: Axios.patch(`https://remotemysql.com:3306/books/${book_id}`, {
       title,
       image,
       genre,
@@ -50,8 +50,8 @@ export const updateBook = (book_id, title, image, genre, desc) => {
 export const deleteBook = (book_id) => {
   return {
     type: 'DELETE_BOOK',
-    // payload: Axios.delete(`http://localhost:8080/books/${book_id}` || `http://remotemysql.com:3306/books/${book_id}`)
-    payload: Axios.delete(`http://remotemysql.com:3306/books/${book_id}`)
+    // payload: Axios.delete(`http://localhost:8080/books/${book_id}` || `https://remotemysql.com:3306/books/${book_id}`)
+    payload: Axios.delete(`https://remotemysql.com:3306/books/${book_id}`)
   }
 }
 
@@ -72,8 +72,8 @@ export const getAvailableBooks = () => {
 export const rentBook = (book_id, user_id) => {
   return {
     type: 'RENT_BOOK',
-    // payload: Axios.patch(`http://localhost:8080/books/rent/${book_id}` || `http://remotemysql.com:3306/books/rent/${book_id}`, {
-    payload: Axios.patch(`http://remotemysql.com:3306/books/rent/${book_id}`, {
+    // payload: Axios.patch(`http://localhost:8080/books/rent/${book_id}` || `https://remotemysql.com:3306/books/rent/${book_id}`, {
+    payload: Axios.patch(`https://remotemysql.com:3306/books/rent/${book_id}`, {
       user_id
     })
   }
@@ -82,8 +82,8 @@ export const rentBook = (book_id, user_id) => {
 export const returnBook = (book_id, user_id) => {
   return {
     type: 'RETURN_BOOK',
-    // payload: Axios.patch(`http://localhost:8080/books/return/${book_id}` || `http://remotemysql.com:3306/books/return/${book_id}`, {
-    payload: Axios.patch(`http://remotemysql.com:3306/books/return/${book_id}`, {
+    // payload: Axios.patch(`http://localhost:8080/books/return/${book_id}` || `https://remotemysql.com:3306/books/return/${book_id}`, {
+    payload: Axios.patch(`https://remotemysql.com:3306/books/return/${book_id}`, {
       user_id
     })
   }
